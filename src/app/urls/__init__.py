80cae6120bd2fbdf5d0d6dfdbe822575d3a36a24
fearsd <rifatfazlutdinov@gmail.com>
2022-06-17 12:08:24 +0500

    initial

---
+++ src/app/urls/__init__.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+
+from django.http import HttpResponse
+
+api = [
+    path('v1/', include('app.urls.v1', namespace='v1')),
+]
+
+def hello(request):
+    return HttpResponse('Everything is ok')
+
+urlpatterns = [
+    path('', hello),
+    path('admin/', admin.site.urls),
+    path('api/', include(api)),
+]

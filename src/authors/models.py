195c45d851ec3f000c37c17d004d5ba159fda446
fearsd <rifatfazlutdinov@gmail.com>
2022-07-26 15:24:48 +0500

    author image field

--- src/authors/models.py
+++ src/authors/models.py
@@ -7,4 +7,4 @@ from app.models import DefaultModel
 
 class Author(DefaultModel):
     name = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(null=True)

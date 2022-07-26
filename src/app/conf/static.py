0991b9cb949fe66cb0cb9a57b769a8c9a863e8d6
fearsd <rifatfazlutdinov@gmail.com>
2022-07-26 15:12:21 +0500

    static files

--- src/app/conf/static.py
+++ src/app/conf/static.py
@@ -1,11 +1,10 @@
 import os
 
 from app.conf.boilerplate import BASE_DIR
-from app.conf.environ import env
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
